2c413acc36422764e706eb963482da98ef56906b
wafistos1 <mameri.wafi@gmail.com>
2019-03-29 11:15:24 +0100

    move macGyver Ok

--- classes.py
+++ classes.py
@@ -40,9 +40,9 @@ class Niveau:
 		"""Méthode permettant d'afficher le niveau en fonction 
 		de la liste de structure renvoyée par generer()"""
 		#Chargement des images (seule celle d'arrivée contient de la transparence)
-		mur = pygame.image.load(structures_mur).convert()
-		depart = pygame.image.load(perso_MacGyver).convert()
-		arrivee = pygame.image.load(Gardien).convert()
+		mur = pygame.image.load(structures_mur).convert_alpha()
+		depart = pygame.image.load(start).convert_alpha()
+		arrivee = pygame.image.load(arrival).convert_alpha()
 		
 		#On parcourt la liste du niveau
 		num_ligne = 0
@@ -67,19 +67,63 @@ class Niveau:
 			
 class Perso:
 	"""Classe permettant de créer un personnage"""
-	def __init__(self, deplacer, niveau):
+	def __init__(self, droite, gauche, haut, bas, niveau):
 		#Sprites du personnage
-		self.deplacer = pygame.image.load(deplacer).convert()
-		
+		self.droite = pygame.image.load(droite).convert_alpha()
+		self.gauche = pygame.image.load(gauche).convert_alpha()
+		self.haut = pygame.image.load(haut).convert_alpha()
+		self.bas = pygame.image.load(bas).convert_alpha()
 		#Position du personnage en cases et en pixels
 		self.case_x = 0
 		self.case_y = 0
 		self.x = 0
 		self.y = 0
 		#Direction par défaut
-		self.direction = self.deplacer
+		self.direction = self.droite
 		#Niveau dans lequel le personnage se trouve 
 		self.niveau = niveau
+	
+	
+	def deplacer(self, direction):
+		"""Methode permettant de déplacer le personnage"""
+		
+		#Déplacement vers la droite
+		if direction == 'droite':
+			#Pour ne pas dépasser l'écran
+			if self.case_x < (nombre_sprite_cote - 1):
+				#On vérifie que la case de destination n'est pas un mur
+				if self.niveau.structure[self.case_y][self.case_x+1] != 'm':
+					#Déplacement d'une case
+					self.case_x += 1
+					#Calcul de la position "réelle" en pixel
+					self.x = self.case_x * taille_sprite
+			#Image dans la bonne direction
+			self.direction = self.droite
+		
+		#Déplacement vers la gauche
+		if direction == 'gauche':
+			if self.case_x > 0:
+				if self.niveau.structure[self.case_y][self.case_x-1] != 'm':
+					self.case_x -= 1
+					self.x = self.case_x * taille_sprite
+			self.direction = self.gauche
+		
+		#Déplacement vers le haut
+		if direction == 'haut':
+			if self.case_y > 0:
+				if self.niveau.structure[self.case_y-1][self.case_x] != 'm':
+					self.case_y -= 1
+					self.y = self.case_y * taille_sprite
+			self.direction = self.haut
+		
+		#Déplacement vers le bas
+		if direction == 'bas':
+			if self.case_y < (nombre_sprite_cote - 1):
+				if self.niveau.structure[self.case_y+1][self.case_x] != 'm':
+					self.case_y += 1
+					self.y = self.case_y * taille_sprite
+			self.direction = self.bas
+
 	
 	
 	
--- macGyver.py
+++ macGyver.py
@@ -17,28 +17,26 @@ from  constates import *
 
 pygame.init()
 
-
+pygame.display.set_caption(fenetre_titre)
 fenetre = pygame.display.set_mode(fenetre_resolution)
+fond = pygame.image.load(fenetre_fond)
 icone = pygame.image.load(fenetre_icon).convert()
 pygame.display.set_icon(icone)
+
+sering = pygame.image.load(seringue).convert()
+
 
 niv = Niveau("niveau.txt")
 niv.generer()
 niv.afficher(fenetre)
 
-fond = pygame.image.load(structures).convert()
 
-"""image_macGyver = pygame.image.load(perso_MacGyver).convert()
-image_gardien = pygame.image.load(Gardien).convert()
-
-fenetre.blit(fond, (0, 0))
-fenetre.blit(image_macGyver, (5, 5))
-fenetre.blit(image_gardien, (460, 80))"""
-
-pygame.display.set_caption(fenetre_titre)
+mac = Perso(perso_MacGyver, perso_MacGyver, perso_MacGyver, perso_MacGyver, niv)
 
 
 
+
+fenetre.blit(sering, (330, 0))
 pygame.display.flip()
 game_over = False 
 
@@ -48,8 +46,19 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-
-    
-
-
-
+        elif event.type == KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_over = True
+            elif event.key == pygame.K_RIGHT:
+                mac.deplacer('droite')
+                print("droite")
+            elif event.key == pygame.K_LEFT:
+                mac.deplacer('gauche')
+            elif event.key == pygame.K_UP:
+                mac.deplacer('haut')
+            elif event.key == pygame.K_DOWN:
+                mac.deplacer('bas')
+    fenetre.blit(fond, (0, 0))
+    niv.afficher(fenetre)
+    fenetre.blit(mac.gauche, (mac.x, mac.y))
+    pygame.display.flip()
