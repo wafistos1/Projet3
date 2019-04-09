d1e309dccdc8a4af453e90913b797bb64a5e87c2
Wafi <mameri.wafi@gmail.com>
2019-04-09 23:47:04 +0100

    pylint level.py and macGyver.py

--- macGyver.py
+++ macGyver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#coding: utf-8
+# coding: utf-8
 
 
 """
@@ -12,23 +12,21 @@ Jeu dans lequel on doit deplac........
 import pygame
 import pygame.locals
 import random
-from  level import *
-from  constantes import *
+from level import *
+from constantes import *
 from perso import *
 
 
-#Initialisation Game
+# Initialisation Game
 pygame.init()
 
-#Color
+# Color
 BLANC = (255, 255, 255)
 
-#font
+# Font
 MY_FONT = pygame.font.SysFont("ubuntu", 50)
 
-
-
-#Diffirentes etapes de la creations du jeu
+# Diffirentes etapes de la creations du jeu
 pygame.display.set_caption(FENETRE_TITRE)
 FENETRE = pygame.display.set_mode(FENETRE_RESOLUTION)
 WINDOW_BACKGROUND = pygame.image.load(GAME_MENU)
@@ -36,79 +34,71 @@ BLACK_BACKGROUND = pygame.image.load(BLACK_BACKGROUND)
 ICON = pygame.image.load(FENETRE_ICON).convert()
 pygame.display.set_icon(ICON)
 
-#Load images of game
+# Load images of game
 ELEMENT_ETHER = pygame.image.load(ETHER).convert_alpha()
 ELEMENT_TUBE = pygame.image.load(TUBE).convert_alpha()
 ELEMENT_AIGUILLE = pygame.image.load(AIGUILLE).convert_alpha()
-ITEM_FOUND = pygame.image.load(MANUFACTURED_SYRINGE).convert_alpha()
+ITEM_trouvé = pygame.image.load(MANUFACTURED_SYRINGE).convert_alpha()
 WINNER = pygame.image.load(WIN_GAME).convert()
 LOOSER = pygame.image.load(GAME_OVER).convert()
 
-
-#Declarations of objects
+# Declarations of objects
 ether_trouver = False
 tube_trouver = False
 aiguille_trouver = False
 item_3 = False
 meet_gardien_macGver = False
 
-
-
-#code for moving the character while holding down the key
+# Code for moving the character while holding down the key
 pygame.key.set_repeat(400, 30)
 
-#Retrieving a list for object positions
+# Retrieving a list for object positions
 niv = Niveau("niveau.txt")
 niv.generer()
-
-
 
 # Condition for menu and game
 menu = True
 game_over = False
 continu_game = True
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 """
 Boucle principale
 """
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-while  menu:
-
-#Creation
-    #Creation list of object
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+while menu:
+    # Creation list of object
     list_object = ["Ether", "Tube", "Aiguille"]
-    #Object_declarations 
+
+    # Object_declarations
     ether_trouver = False
     tube_trouver = False
     aiguille_trouver = False
     item_3 = False
     meet_gardien_macGver = False
 
-
-
+    # Create level
     niv.afficher(FENETRE)
+
+    # Create a character
     mac = Perso(PERSO_MACGYVER, niv)
     pygame.display.flip()
 
-    print(niv.position_elem)
+    # Blit objects in labyrinthe
     position_ether, position_tube, position_aiguille = random.sample(niv.position_elem, 3)
-    print(position_aiguille, position_ether, position_tube)
-
-    #Blit objects in labyrinthe
     FENETRE.blit(ELEMENT_ETHER, tuple(position_ether))
     FENETRE.blit(ELEMENT_TUBE, tuple(position_tube))
     FENETRE.blit(ELEMENT_AIGUILLE, tuple(position_aiguille))
 
     pygame.display.flip()
-    game_over = False #condition pour une nouvelle partie
-    continu_game = True #conditon pour nouvelle partie
 
-
+    game_over = False  # condition pour une nouvelle partie
+    continu_game = True  # conditon pour nouvelle partie
 
     while continu_game:
         pygame.time.Clock().tick(10)
-        FENETRE.blit(WINDOW_BACKGROUND, (50, 50))
+        FENETRE.blit(WINDOW_BACKGROUND, (65, 90))
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -121,10 +111,9 @@ while  menu:
                 elif event.key == pygame.K_c:
                     continu_game = False
 
-
-    #main loop of game
+    # Main loop of game
     while not game_over:
-        pygame.time.Clock().tick(30)# Framerate 30
+        pygame.time.Clock().tick(30)  # Framerate 30
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -132,7 +121,7 @@ while  menu:
                 continu_game = False
                 game_over = True
 
-            #Control du personnage
+            # Control du personnage
             elif event.type == KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     game_over = True
@@ -145,17 +134,17 @@ while  menu:
                 elif event.key == pygame.K_DOWN:
                     mac.deplacer('bas')
 
-    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
-        #character control
+    #  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+        # Character control
         FENETRE.blit(BLACK_BACKGROUND, (0, 0))
         niv.afficher(FENETRE)
 
-        #Logic of the display of objects
+        # Logic of the display of objects
         if position_ether != (mac.x, mac.y) and not ether_trouver:
             FENETRE.blit(ELEMENT_ETHER, tuple(position_ether))
         else:
             if not ether_trouver:
-                animation(FENETRE, "Ether found", MY_FONT, )
+                animation(FENETRE, "Éther trouvé", MY_FONT, )
                 list_object.remove("Ether")
                 if list_object:
                     animation(FENETRE, f" {len(list_object)} Objets restant", MY_FONT)
@@ -165,7 +154,7 @@ while  menu:
             FENETRE.blit(ELEMENT_TUBE, tuple(position_tube))
         else:
             if not tube_trouver:
-                animation(FENETRE, "Tube found", MY_FONT, )
+                animation(FENETRE, "Tube trouvé", MY_FONT, )
                 list_object.remove("Tube")
                 if list_object:
                     animation(FENETRE, f" {len(list_object)} Objets restant", MY_FONT)
@@ -176,17 +165,18 @@ while  menu:
         else:
             if not aiguille_trouver:
 
-                animation(FENETRE, f"Aiguille found", MY_FONT, )
+                animation(FENETRE, f"Aiguille trouvée", MY_FONT, )
                 list_object.remove("Aiguille")
                 if list_object:
                     animation(FENETRE, f" {len(list_object)} Objets restant", MY_FONT)
                 aiguille_trouver = True
-        # Verifications si tous les items on ete trouve
-        if tube_trouver and ether_trouver and aiguille_trouver  and not item_3:
-            affichage(FENETRE, ITEM_FOUND)
+
+        # Verifications si tous les objets on ete trouves
+        if tube_trouver and ether_trouver and aiguille_trouver and not item_3:
+            affichage(FENETRE, ITEM_trouvé)
             item_3 = True
 
-        #Meet prison guard
+        # Meet prison guard
         if mac.x == 360 and mac.y == 420 and not meet_gardien_macGver:
             meet_gardien_macGver = True
             if item_3:
@@ -195,13 +185,11 @@ while  menu:
             else:
                 affichage(FENETRE, LOOSER)
 
-
         if niv.structure[mac.case_y][mac.case_x] == 'a':
             game_over = True
-            niv.position_elem = [] #Initialisation the list of positions of Objects at 0
-            #menu = False
+            niv.position_elem = []  # Initialisation the list of positions of Objects at 0
 
         FENETRE.blit(mac.personnage, (mac.x, mac.y))
-        #Condition of collision
-    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Condition of collision
+    #  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         pygame.display.flip()
